943e0f5028ea3459b83be44353164851cfd3f510
Bahadir Sonmez <bahadirovski@macbook-pro.local>
2020-11-30 10:15:55 +0300

    First Commit

---
+++ TestFramework/TestFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TestFramework
+//
+//  Created by Bahadir Sonmez on 30.11.2020.
+//
+
+import Foundation
+
+public class Service {
+
+    private init() {}
+
+    public static func doSomething() -> String {
+        return "Did some stuff, here you go homeboi!"
+    }
+}
+
+ 
